55d1ff1387a20fa4459d67699227658b7dd771ed
Justin Maronde <myemailisjustin@me.com>
2024-09-04 07:54:14 -0400

    Initial Commit

---
+++ HelloBudgets/HelloBudgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloBudgetsApp.swift
+//  HelloBudgets
+//
+//  Created by Justin Maronde on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloBudgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
